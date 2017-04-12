3c5f22398c8766216574ae77e0915f8e0c850df0
IMrMachineI <fubu96meex@gmail.com>
2017-04-12 18:51:48 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vectores
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int tamaño = 0;
+        Vector nuevoVector;
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            tamaño = Convert.ToInt32(txtCrear.Text);
+            nuevoVector = new Vector(tamaño);
+        }
+
+        private void btnLlenar_Click(object sender, EventArgs e)
+        {
+            int numInicial = Convert.ToInt32(txtnumInicial.Text);
+            nuevoVector.llenarVector(numInicial);
+        }
+        private void btnMostrarDatos_Click(object sender, EventArgs e)
+        {
+            txtMostrar.Text = nuevoVector.ToString();
+        }
+
+        private void btnInvertir_Click(object sender, EventArgs e)
+        {
+            nuevoVector.invertir();
+        }
+
+        private void btnGirarAbajo_Click(object sender, EventArgs e)
+        {
+            nuevoVector.girarAbajo();
+        }
+
+        private void btnGirarArriba_Click(object sender, EventArgs e)
+        {
+            nuevoVector.girarArriba();
+        }
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectores
+{
+    class Vector
+    {
+        private int tamaño { get; }
+        int[] vector;
+        public Vector(int tamaño)
+        {
+            this.tamaño = tamaño;
+            vector = new int[tamaño];
+        }
+        public void llenarVector(int numInicial)
+        {
+            for (int i = 0; i < tamaño; i++)
+            {
+                vector[i] = numInicial;
+                numInicial++;
+            }
+        }
+        public void girarArriba()
+        {
+            int varTemporal;
+            for(int i = tamaño;i>0;i--)
+            {
+                varTemporal = vector[i-1];
+                vector[i - 1] = vector[tamaño - 1];
+                vector[tamaño - 1] = varTemporal;
+            }
+        }
+        public void girarAbajo()
+        {
+            int varTemporal;
+            for (int i = 0; i < tamaño; i++)
+            {
+                varTemporal = vector[i];
+                vector[i] = vector[tamaño - 1];
+                vector[tamaño - 1] = varTemporal;
+            }
+        }
+        public void invertir()
+        {
+            int varTemporal;
+            int tamaño = vector.Length - 1;
+            int i = 0;
+            while ((vector.Length - i)>i)
+            {
+                varTemporal = vector[i];
+                vector[i] = vector[tamaño - i];
+                vector[tamaño - i] = varTemporal;
+                i++;
+            }
+        }
+        public override string ToString()
+        {
+            string regresar = "";
+            for (int i = 0;i<tamaño;i++)//Controlar posiciones del vector
+            {
+                regresar = regresar + "["+i+"]="+vector[i]+"\r\n";
+            }
+            return regresar;
+        }
+    }
+}
